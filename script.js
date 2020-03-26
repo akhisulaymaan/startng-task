895ba8981727acd734c5f7652281a86086bf3f3c
akhisulaymaan <54278218+akhisulaymaan@users.noreply.github.com>
2020-03-26 11:54:12 +0100

    javascript task

---
+++ script.js
@@ -0,0 +1,9 @@
+let myName = "Sulaymaan";
+ console.log("My name is: " + myName);
+ const courses = ["HTML", "CSS", "Javascript"];
+ for (let i=0; i<=200; i++) {
+   if (i%2===1) {
+     courses.push(i);
+   }
+ }
+ console.log(courses)
